990fdc895b5f3057a951d5c04b61d340ca796a03
Blake Archer <blarcher31@gmail.com>
2020-10-15 20:55:07 -0500

    Includes a summoner route that shows a JSON message, and summoner/:id route that allows you to read the summoner name in JSON format

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import {getTestRoutes} from './testRoutes'
-
+import {getSummoner} from './matchHistoryRoutes'
 
 
 function getRoutes() {
   const router = express.Router()
   router.use('/test', getTestRoutes())
+  router.use('/test-run', getSummoner())
   return router
 }
+
 
 export {getRoutes}
