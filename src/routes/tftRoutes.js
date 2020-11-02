b603cc16e467988c6e6b8d78dce242a4fb12b8ae
Blake Archer <blarcher31@gmail.com>
2020-11-02 11:38:02 -0600

    Update to Send JSON summoner info to the browser.

--- src/routes/tftRoutes.js
+++ src/routes/tftRoutes.js
@@ -1,13 +1,21 @@
 import express from 'express'
+import riotApiClient from '../services/riotApiClient'
 
 function getTFTRoutes(){
     const router = express.Router()
-    router.get('/summoner/:summonerName', getSummonerHandler)
+    router.get('/v1/summoner/:summonerName', getSummonerHandler)
     return router
 }
 
+
 async function getSummonerHandler(req, res){
-    res.send("Welcome " + req.params.summonerName + "!!")
-}
+    const summonerName = req.params.summonerName;
+    try{
+        const summonerInfo = await riotApiClient.fetchTFTSummonerInfo(summonerName);
+        res.send(summonerInfo)
+    } catch (e) {
+      return res.status(500).send(`Unable to fetch summoner ${summonerName}: ${e.message}`)
+      }
+    }
 
 export {getTFTRoutes}
---
+++ src/services/riotApiClient.js
@@ -0,0 +1,31 @@
+import logger from 'loglevel' 
+import config from 'config'
+import axios from 'axios'
+
+class RiotApiClient {
+    async fetchTFTSummonerInfo(summonerName) {
+      const url = `/tft/summoner/v1/summoners/by-name/${summonerName}`
+  
+      let response;
+      try {
+        response = await axios.get(url,
+            {
+              baseURL: `${config.get('riot.baseURL')}`,
+              headers: {
+                'X-Riot-Token': `${config.get('riot.apiKey')}`
+              }
+            }
+        )
+        return response.data
+      } catch (err) {
+        logger.error(`An error occurred attempting to fetch from ${response.config.url}, ${err.message}`)
+        throw err
+      }
+    }
+  }
+
+        
+    
+
+
+export default new RiotApiClient()
