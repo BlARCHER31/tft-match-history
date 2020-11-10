a68b43b96ab3f5c486c0ea286d502fbeff4ccc62
Blake Archer <blarcher31@gmail.com>
2020-11-10 14:50:13 -0600

    updates allowing to search for recent matches

--- src/routes/tftRoutes.js
+++ src/routes/tftRoutes.js
@@ -5,6 +5,7 @@ import riotApiClient from '../services/riotApiClient'
 function getTFTRoutes(){
     const router = express.Router()
     router.get('/v1/summoner/:summonerName', getSummonerHandler)
+    router.get('/v1/match/:summonerName/:count', getMatchHistoryHandler)
     return router
 }
 
@@ -14,12 +15,30 @@ async function getSummonerHandler(req, res){
     try{
         const iconIdURL = await riotApiClient.getLoLPatchVersions();
         const summonerInfo = await riotApiClient.fetchTFTSummonerInfo(summonerName);
+        
         const iconID = summonerInfo.profileIconId
+
         summonerInfo['profileIconId'] = `http://ddragon.leagueoflegends.com/cdn/${iconIdURL}/img/profileicon/${iconID}.png`;
-        res.send(summonerInfo.profileIconId)
+        res.send(summonerInfo)
     } catch (e) {
       return res.status(500).send(`Unable to fetch summoner ${summonerName}: ${e.message}`)
       }
-    }
+}
+
+async function getMatchHistoryHandler(req, res) {
+    const summonerName = req.params.summonerName;
+    const count = req.params.count
+    try {
+        const summonerInfo = await riotApiClient.fetchTFTSummonerInfo(summonerName);
+        const puuid = summonerInfo.puuid
+        const matchInfo = await riotApiClient.getRecentMatches(puuid, count)
+        res.send(matchInfo)
+    } catch (e) {
+        return res.status(500).send(`Unable to fetch summoner Match Data for  ${summonerName}: ${e.message}`)
+        }
+}
+
+
+
 
 export {getTFTRoutes}
--- src/services/riotApiClient.js
+++ src/services/riotApiClient.js
@@ -2,6 +2,7 @@ import logger from 'loglevel'
 import config from 'config'
 import axios from 'axios'
 
+
 class RiotApiClient {
     async fetchTFTSummonerInfo(summonerName) {
       const url = `/tft/summoner/v1/summoners/by-name/${summonerName}`
@@ -34,6 +35,26 @@ class RiotApiClient {
       return version[0];
   
    }
+
+   async getRecentMatches(puuid, count) {
+     const puuidURL = `/tft/match/v1/matches/by-puuid/${puuid}/ids?count=${count}` 
+     let response;
+     try{
+      response = await axios.get(puuidURL,
+        {
+          baseURL: config.get('riot.matchURL'),
+          headers: {
+            'X-Riot-Token': config.get('riot.apiKey')
+          }
+        }
+    )
+    return response.data
+        
+    } catch (err) {
+      logger.error(`An error occurred attempting to fetch from ${response.config.puuidURL}, ${err.message}`)
+      throw err
+    }
+  }
 }
 
 
